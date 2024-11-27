cf802a6fba13462661ef5b6c890ea4a0a08a2e0d
pengbo <pbstar@sina.cn>
2024-11-27 16:12:23 +0800

    更新vite6以及其他依赖版本

--- packages/template/vite.config.js
+++ packages/template/vite.config.js
@@ -5,13 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler', // or 'modern'
-      },
-    },
-  },
   plugins: [
     vue(),
   ],
